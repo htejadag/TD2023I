323744c3f7cac7211c8e1056432cd3bb3b30b7c4
Hernando Tejada <htejadag25@gmail.com>
2023-05-19 19:30:14 -0500

    Ejemplo de manejo de excepciones

--- AppPrueba/Program.cs
+++ AppPrueba/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Serilog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,9 +11,99 @@ namespace AppPrueba
     {
         static void Main(string[] args)
         {
-            //asdasd
-            Console.WriteLine("Bye, World!");
-            Console.WriteLine("Hola, Mundo!");
+            // Configuración del logger
+            Log.Logger = new LoggerConfiguration()
+                            .WriteTo.File("Tejada.txt")
+                            .CreateLogger();
+
+            //Console.WriteLine("Bye, World!");
+            //Console.WriteLine("Hola, Mundo!");
+            //Saludar();
+            //Convertir();
+            Convertir2();
+        }
+
+        public static void Saludar()
+        {
+            try
+            {
+                // Acá pueden ir los Log Informativos y de Advertencia
+                Log.Information("Vamos a dividir dos numeros");
+
+                int a = 10;
+                int b = 0;
+                int c = a / b;
+
+                Log.Information("La división se realízó correctamente");
+
+            }
+            catch (Exception ex)
+            {
+                //Acá va el Log de Error
+                Log.Error("No se pudo dividir, porqué " + ex.Message);
+            }
+
+        }
+
+        public static void Convertir()
+        {
+            try
+            {
+                // Acá pueden ir los Log Informativos y de Advertencia
+                Log.Information("Vamos a dividir dos numeros");
+
+                Int16 b;
+                Console.WriteLine("Ingrese un número int16");
+                string a = Console.ReadLine();
+                b = Int16.Parse(a);
+                Console.WriteLine("El número ingresado es " + b);
+
+
+                Log.Information("La división se realízó correctamente");
+
+            }
+            catch (Exception ex)
+            {
+                //Acá va el Log de Error
+                Log.Error("No se pudo dividir, porqué " + ex.Message);
+            }
+
+        }
+
+        public static void Convertir2()
+        {
+            try
+            {
+                // Acá pueden ir los Log Informativos y de Advertencia
+                Log.Information("Vamos a dividir dos numeros");
+
+                Int16 b;
+                Console.WriteLine("Ingrese un número");
+                string a = Console.ReadLine();
+                b = Int16.Parse(a);
+                Console.WriteLine("El número ingresado es " + b);
+
+
+                Log.Information("La división se realízó correctamente");
+
+            }
+            catch (ArgumentNullException ex)
+            {
+                Log.Error("ArgumentNullException " + ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Log.Error("FormatException " + ex.Message);
+            }
+            catch (OverflowException ex)
+            {
+                Log.Error("OverflowException " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Exception " + ex.Message);
+            }
+
         }
     }
 }
